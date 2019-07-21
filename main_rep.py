62ed377f0a82807ad0612002196ed63a8d319c1d
NIkita.Savchenko <nikita-otradniy@yandex.ru>
2019-07-22 01:09:21 +0400

    "Commit for add unit and insert in DB"

--- main_rep.py
+++ main_rep.py
@@ -1,13 +1,18 @@
 import sqlite3
+from datetime import datetime
 from logging import basicConfig, error, info
 import kivy.app
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+import kivy.uix.label
 from kivy.logger import Logger
 from kivy.uix.modalview import ModalView
 import kivy.uix.textinput
+from kivy.uix.popup import Popup
+import kivy.uix.button
+import re
 
 import conf_rep
+import datepicker
 
 
 def sql_connection_check():
@@ -21,16 +26,34 @@ def sql_connection_check():
         conn.close()
 
 
-def sql_table():
+def sql_table_select():
     conn = sqlite3.connect('rep.db')
     info("Соединение с БД успешно установлено")
     cursor_table = conn.cursor()
     try:
-        cursor_table.execute("select * from list_lessons")
+        cur_select = cursor_table.execute("select * from list_lessons")
+        return cur_select
     except:
         cursor_table.execute(
-            "create table list_lessons (id integer PRIMARY KEY, themas text, house_ex text, date_lessons text, check_salary numeric)")
-        conn.commit()
+            "create table list_lessons (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "
+            "name text, themas text, date_lessons text, salary numeric, check_salary numeric)")
+
+def sql_table_insert(username,Unit, Cost, date, hour):
+    conn = sqlite3.connect('rep.db')
+    info("Соединение с БД успешно установлено")
+    cursor_table = conn.cursor()
+    try:
+        hour_1 = re.sub(r'\]', ':', str(hour))
+        Logger.info('час: {}'.format(hour_1))
+        date_str = datetime.combine(date, hour)
+        Logger.info(date_str)
+        date_str_1 = date_str.strftime("%d/%m/%y %H:%M")
+        cur_insert = "insert into list_lessons values ({}, {}, {}, {}, {}, null)".format(username, str(Unit), date_str_1, Cost)
+        Logger.info(cur_insert)
+        cur_insert_result = cursor_table.execute(cur_insert)
+        return cur_insert_result
+    except Exception as e:
+        Logger.error(str(e))
 
 
 class MyLayout():
@@ -43,13 +66,26 @@ class RepCheckAppBoxLayout(BoxLayout):
     info("Главный виджет")
     def child_save(self):
         Logger.info('Запускаем окно для добавления записи')
-        view = ViewAddEvent()
+        view = ViewAddEvent(size_hint=(1, 1))
         view.open()
 
 class ViewAddEvent(ModalView):
     def close_child(self):
         Logger.info('Закрываем окно для добавления записи')
         self.dismiss()
+    def add_record(self, username,Unit, Cost, date, hour):
+        Logger.info('добавляем атрибуты')
+        str_log = "Получены атрибуты: {}, '{}', {}, {}, {}".format(username,Unit, Cost, date, str(hour))
+        Logger.info(str_log)
+        if sql_table_insert(username,Unit, Cost, date, str(hour)):
+            popup = Popup(title='Запись добавлена',
+                          content=kivy.Button(text='закрыть окно'))
+            popup.open()
+
+
+class SchedulerView(ModalView):
+    pass
+
 
 class RepCheckApp(kivy.app.App):
     def build(self):
@@ -61,5 +97,5 @@ if __name__ == '__main__':
     basicConfig(filename="reper.txt", level=conf_rep.log_level)
     info("Файл логов начат")
     sql_connection_check()
-    sql_table()
+    sql_table_select()
     RepCheckApp().run()
